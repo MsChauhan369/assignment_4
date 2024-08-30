b8c5ee39c8851f165aca493ebabeeae5670bad30
m.s.chauhan369 <102317404+MsChauhan369@users.noreply.github.com>
2024-08-31 00:50:28 +0500

    Update exercise_4.py

--- exercise_4.py
+++ exercise_4.py
@@ -7,57 +7,52 @@ print(f"Hello, my name is {name} and I am {age} years old.")
 
 
 # 2. Strings:
+texting = "Hello, My Dear Sir Zia Khan!"
+print(texting.upper())  # Output: HELLO, WORLD!
+print(texting.lower())  # Output: hello, world!
+print(texting.replace("world", "there"))  # Output: Hello, there!
 
-text = "Hello, world!"
-print(text.upper())  # Output: HELLO, WORLD!
-print(text.lower())  # Output: hello, world!
-print(text.replace("world", "there"))  # Output: Hello, there!
 
 # 3. f-Strings:
 
-pi = 3.14159
-radius = 5
+name = "Muhammad Shahzad"
+age = 35
 
-area = pi * radius**2
-print(f"The area of a circle with radius {radius} is {area:.2f}")  # Output: The area of a circle with radius 5 is 78.54
-
+print(f"Hello, my name is {name} and I am {age} years old.")
 # 4. Operators:
+x = 7
+y = 4
 
-x = 5
-y = 3
-
-print(x + y)  # Output: 8
-print(x - y)  # Output: 2
-print(x * y)  # Output: 15
-print(x / y)  # Output: 1.6666666666666667
-print(x % y)  # Output: 2
-print(x ** y)  # Output:  
+print(x + y)  # Output: 11
+print(x - y)  # Output: 3
+print(x * y)  # Output: 28
+print(x / y)  # Output: 1.75
+print(x % y)  # Output: 3
+print(x ** y)  # Output: 2401
 
 # 5. Lists:
 
-fruits = ["apple", "banana", "orange"]
-print(fruits[0])  # Output: apple
-fruits.append("grape")
-print(fruits)  # Output: ['apple', 'banana', 'orange', 'grape']
-fruits.remove("banana")
-print(fruits)  # Output: ['apple', 'orange', 'grape']
-
+fruits = ["grape", "mango", "cherry"]
+print(fruits[0])  # Output: grape
+fruits.append("orange")
+print(fruits)  # Output: ['grape', 'mango', 'cherry', 'orange']
+fruits.remove("mango")
+print(fruits)  # Output: ['grape', 'cherry', 'orange']
 
 # 6. Tuples:
 
-coordinates = (10, 20)
-print(coordinates[0])  # Output: 10
-# coordinates[0] = 5  # This will raise an error as tuples are immutable
-
+numbers = (5, 19, 40)
+print(numbers[0])  # Output: 5
+#numbers[0] = 5  # This will raise an error as tuples are immutable
 
 # 7. For Loops:
 
-for i in range(5):
-    print(i)  # Output: 0 1 2 3 4
+for i in range(6):
+    print(i)  # Output: 0 1 2 3 4 5
 
-numbers = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5, 6]
 for number in numbers:
-    print(number)  # Output: 1 2 3 4 5
+    print(number)  # Output: 1 2 3 4 5 6
 
 
 # 8. Input Handling:
